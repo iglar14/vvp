90e2d148a75e73bd88bfaea32bef82c7d04e7396
igor.larionov <igor.larionov@cpslabs.net>
2018-03-11 12:02:02 +0300

    Упрощенный вариант удаления пробелов, с комментами

--- lec_state_machine/reb1.pas
+++ lec_state_machine/reb1.pas
@@ -1,19 +1,21 @@
 PROGRAM RemoveExtraBlanks;
 VAR
   Ch: CHAR;
-BEGIN
-  Ch := ' ';
+BEGIN {RemoveExtraBlanks}
+  Ch := ' '; {»нициализаци€}
+  
+  {„итаем пробелы в начале строки}
   WHILE (NOT EOLN) AND (Ch = ' ')
   DO
-    BEGIN
-      READ(Ch);
-      IF Ch <> ' '
-      THEN
-        WRITE(Ch);
-    END;
+    READ(Ch);
+  IF Ch <> ' '
+  THEN
+    WRITE(Ch);
+    
   WHILE NOT EOLN
   DO
     BEGIN
+      {„итаем слово и выводим его}
       WHILE (NOT EOLN) AND (Ch <> ' ')
       DO
         BEGIN
@@ -22,14 +24,16 @@ BEGIN
           THEN
             WRITE(Ch)
         END;
+        
+      {„итаем пробелы до следующего слова или до конца строки}
       WHILE (NOT EOLN) AND (Ch = ' ')
       DO
-        BEGIN
-          READ(Ch);
-          IF Ch <> ' '
-          THEN
-            WRITE(' ', Ch);
-        END;
+        READ(Ch);
+        
+      {≈сли встретилс€ не пробел, то это начало следующего слова}
+      IF Ch <> ' '
+      THEN
+        WRITE(' ', Ch); {ставим пробел перед словом}
     END;
   WRITELN;
-END.
+END.  {RemoveExtraBlanks}
